98f4c5b8b71b3c6caeba60336699005ae2fba431
Alexander Perepelitsa <aa@giftery.ru>
2018-04-09 16:26:57 +0300

    add: VAULT_CACHE_FILE

--- src/func.php
+++ src/func.php
@@ -5,41 +5,65 @@ function secEnv($name)
 	$value = getenv($name);
 	if (strpos($value, "VAULT:") === 0)
 	{
-		$ch = curl_init();
+		$value   = substr($value, 6);
 
-		$url     = (string) getenv('VAULT_ADDR').'/v1/secret/'.substr($value, 6);
-		$token   = (string) getenv('VAULT_TOKEN');
-		$timeout = (int) getenv('VAULT_TIMEOUT')>0 ? (int) getenv('VAULT_TIMEOUT') : 100;
+		$cache      = [];
+		$cache_ttl  = getenv('VAULT_CACHE_TTL')>0 ? (int) getenv('VAULT_CACHE_TTL') : 300;
+		$cache_file = getenv('VAULT_CACHE_FILE') ? (string) getenv('VAULT_CACHE_FILE') : sys_get_temp_dir() . DIRECTORY_SEPARATOR .'secenv.json';
 
-		curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-Vault-Token: '.$token]);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, $timeout);
-
-		$res = curl_exec($ch);
-
-		if ($res === false)
+		if(file_exists($cache_file))
 		{
-			error_log('secEnv(): curl_error: ' . curl_error($ch));
-			$value = false;
+			$cache = json_decode(file_get_contents($cache_file), true);
+			$cache_ttl+= filemtime($cache_file);
+		}
+
+		if (array_key_exists($value, $cache) && $cache_ttl > time()) {
+			$value = $cache[$value];
 		}
 		else
 		{
-			$r = json_decode($res, true);
+			$ch = curl_init();
 
-			if (isset($r['data']['value']))
+			$token   = (string) getenv('VAULT_TOKEN');
+			$timeout = (int) getenv('VAULT_TIMEOUT')>0 ? (int) getenv('VAULT_TIMEOUT') : 100;
+			$url     = (string) getenv('VAULT_ADDR').'/v1/secret/'.$value;
+
+			curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-Vault-Token: '.$token]);
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, $timeout);
+			
+			$res = curl_exec($ch);
+			
+			if ($res === false)
 			{
-				$value = (string) $r['data']['value'];
+				error_log('secEnv(): curl_error: ' . curl_error($ch));
+				$value = array_key_exists($value, $cache) ? $cache[$value] : false;
 			}
 			else
 			{
-				error_log('secEnv(): no data. Vault response: ' . $res);
-				$value = false;
-			}
-		}
+				$r = json_decode($res, true);
+				
+				if (isset($r['data']['value']))
+				{
+					if (!array_key_exists($value, $cache) || $cache[$value] != $r['data']['value'])
+					{
+						$cache[$value] = (string) $r['data']['value'];
+						file_put_contents($cache_file, json_encode($cache, JSON_UNESCAPED_UNICODE), LOCK_EX);
+					}
 
-		curl_close($ch);
+					$value = (string) $r['data']['value'];
+				}
+				else
+				{
+					error_log('secEnv(): no data. Vault response: ' . $res);
+					$value = array_key_exists($value, $cache) ? $cache[$value] : false;
+				}
+			}
+			
+			curl_close($ch);
+		}
 	}
 
 	return $value;
